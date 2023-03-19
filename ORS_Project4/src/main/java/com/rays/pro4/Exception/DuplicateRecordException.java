29eaadc19439ba7cc7f9a943dbb30b045bc1e7dc
vamanpatel <vamanpatel1@gmail.com>
2023-03-19 15:50:00 +0530

    ors

---
+++ ORS_Project4/src/main/java/com/rays/pro4/Exception/DuplicateRecordException.java
@@ -0,0 +1,17 @@
+package com.rays.pro4.Exception;
+
+/**
+ * DuplicateRecordException thrown when a duplicate record occurred.
+ * 
+ * @author Vaman Patel
+ *
+ */
+
+public class DuplicateRecordException extends Exception {
+
+	public DuplicateRecordException(String msg){
+		super(msg);
+	}
+
+	
+}
